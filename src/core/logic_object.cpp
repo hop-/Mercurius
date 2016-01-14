f9ae69994e983659ee84174eaba3e4933ea9ec64
tigrangs <programtg@yahoo.com>
2016-01-14 22:21:15 +0400

    removed empty lines

--- src/core/logic_object.cpp
+++ src/core/logic_object.cpp
@@ -3,18 +3,11 @@
 namespace Core
 {
 
-
 void LogicObject::update()
 {
-<<<<<<< HEAD
-    for (Component* component : m_components) {
-        assert(component != 0);
-        component->update();
-=======
     for (auto& pair : m_components) {
         assert(0 != pair.second);
         pair.second->update();
->>>>>>> cc34d4e70a4142408082416afcf5d1e4f7a8fa3a
     }
 }
 
@@ -27,17 +20,10 @@ void LogicObject::addComponent(Component* component)
 
 void LogicObject::init()
 {
-<<<<<<< HEAD
-    for (Component* component : m_components) {
-        assert(component != 0);
-        component->init();
-=======
     for (auto& pair : m_components) {
         assert(0 != pair.second);
         pair.second->init();
->>>>>>> cc34d4e70a4142408082416afcf5d1e4f7a8fa3a
     }
 }
-
 
 } // namespace Core
