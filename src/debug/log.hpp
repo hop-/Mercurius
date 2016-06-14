36e8b6f901be19e15e401c33dc31e9dd36e5fe78
hop- <hovhannespalyan@openmailbox.org>
2016-06-14 20:58:25 +0400

    Added Debug::Log::print static member function to print debug info in terminal

--- src/debug/log.hpp
+++ src/debug/log.hpp
@@ -1,6 +1,10 @@
 #ifndef _DEBUG_LOG_HPP_
 #define _DEBUG_LOG_HPP_
 
+#include <core/position.hpp>
+
+#include <iostream>
+
 namespace Debug
 {
 
@@ -8,6 +12,33 @@ class Log
 {
 
 // use static functions to implement logging mechanism
+#ifdef DM_NO_DEBUG
+public:
+    template <class T>
+    static void print(T) {}
+
+    template <class T>
+    static void print(std::string, T) {}
+#else
+public:
+    template <class T>
+    static void print(const T& t)
+    {
+        std::cout << t << std::endl;
+    }
+
+    static void print(const Core::Position& p)
+    {
+        std::cout << p.x() << ", " << p.y() << std::endl;
+    }
+
+    template <class T>
+    static void print(std::string s, const T& t)
+    {
+        std::cout << s << " ";
+        print(t);
+    }
+#endif // DM_NO_DEBUG
 
 };
 
