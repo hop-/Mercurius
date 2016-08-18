c503285912ee6e32eb872b47bf4cc7caa7e4c71d
tigrangs <programtg@yahoo.com>
2016-08-18 11:48:41 -0700

    Some redesing and cleanup in services

---
+++ src/core/controller.hpp
@@ -0,0 +1,17 @@
+#ifndef _CORE_CONTROLLER_HPP_
+#define _CORE_CONTROLLER_HPP_
+
+#include <base/singleton.hpp>
+
+#include <map>
+
+namespace Core
+{
+
+class Controller : public Base::Singleton<Controller>
+{
+};
+
+} // end of namespace Core
+
+#endif // _CORE_CONTROLLER_HPP_
--- src/core/service.hpp
+++ src/core/service.hpp
@@ -1,12 +1,11 @@
 #ifndef _CORE_SERVICE_HPP_
 #define _CORE_SERVICE_HPP_
 
-#include <base/singleton.hpp>
+#include "controller.hpp"
 
 namespace Core {
 
-template <typename T>
-class Service : public Base::Singleton<T>
+class Service : public Base::TypedBase
 {
 public:
     Service()
@@ -32,13 +31,36 @@ public:
         // TODO stop from controller by checking dependencies
     }
 
+public:
+    virtual int getType()
+    {
+        return m_id;
+    }
+
+protected:
+    virtual void register_dependencies()
+    {}
+
 protected:
     virtual void start() = 0;
     virtual void stop() = 0;
 
-public:
-    virtual void register_dependencies()
-    {}
+private:
+    Base::TypedBase::ID m_id;
+};
+
+template <typename T>
+class Singleton : public Service
+                , public Base::Singleton<T>
+{
+protected:
+    template <typename D, typename S = T>
+    static bool create_dependency()
+    {
+        Controller* c = Controller::getInstance();
+        assert(0 != c);
+        c->register_dependecy(S::getInstance(), D::getInstance());
+    }
 };
 
 } // end of namespace Core
